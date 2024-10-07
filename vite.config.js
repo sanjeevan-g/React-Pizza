351399dbeffe0c17881652dea69b8af7e6118470
sanjeevan <sanjeevan.g0@gmail.com>
2024-10-07 15:29:56 +0530

    eslint update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
-
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
-});
+  plugins: [react()],
+})
